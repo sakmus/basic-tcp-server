972172bc38fe2b3a4387a5f588d3bd893882cb58
Sakeef Mushfique <sakeefmushfique@protonmail.com>
2025-05-26 06:41:51 +0600

    cleaned up the server code to use only functions

--- src/server.c
+++ src/server.c
@@ -22,43 +22,10 @@ int main(int argc, char *argv[]) {
 	return 1;
     }
 
-    // Server main loop
-    while (1) {
-        // Connect to the client
-        struct sockaddr_in client_addr;
-        socklen_t client_addr_len = sizeof(client_addr);
-        int client_fd = accept(server_fd, (struct sockaddr *) &client_addr, &client_addr_len);
-        if (client_fd < 0) {
-            printf("Couldn't connect to the client... %s\n", strerror(errno));
-            continue;
-        }
+    // Run the main server
+    run_server();
 
-        // Allocate memory in heap for client information
-        client_info_t* client_info = malloc(sizeof(client_info_t));
-        if (client_info == NULL) {
-            printf("Failed to allocate memory for client information...\n");
-            close(client_fd);
-            continue;
-        }
-
-        // Store client information
-        client_info->client_fd = client_fd;
-        client_info->client_addr = client_addr;
-
-        // Create a client thread
-        pthread_t thread_id;
-        if (pthread_create(&thread_id, NULL, handle_client, (void*)client_info) != 0) {
-            printf("Couldn't create a thread for client... %s\n", strerror(errno));
-            close(client_fd);
-            free(client_info);
-            continue;
-        }
-
-        // Detach the client thread for independence
-        pthread_detach(thread_id);
-
-    }
-
+    // Close the server even though it won't come here due to infinite loop
     close(server_fd);
 
     return 0;
